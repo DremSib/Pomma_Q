b2de70a3a6f981f4c62cf655fe8c0c4f7a15d539
DremSib <124364231+DremSib@users.noreply.github.com>
2023-09-19 14:51:07 +0200

    L2
    Lesson №2

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-name = input()
-print(name)
-name = input()
-print(name)
-name = input()
-print(name)
+def exponentiation(number, degree):
+  
+  # Код функции возведения в степень.
+  # Нельзя использовать выражение вида number**degree. 
+  # В теле функции следует использовать переменные number и degree,
+  # где number -- основание, degree -- степень, в которое возводится основание.
+  # Результат функции -- переменная result.
+
+  # ПРИМЕЧАНИЕ: Функция должна включать в себя ТОЛЬКО целочисленные значения int (-32768 ... 32767).
+  
+  return result
+
+a = int(input("number = "))
+b = int(input("degree = "))
+
+print(exponentiation(a, b))
